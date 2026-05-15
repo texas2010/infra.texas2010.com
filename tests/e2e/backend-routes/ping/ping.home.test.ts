b5c4c4cc6d363c2d16fd3c4fa507fe53632d8aa1
texas2010 <9701609+texas2010@users.noreply.github.com>
2026-05-15 18:20:19 -0500

    Update ping.home.test.ts

--- tests/e2e/backend-routes/ping/ping.home.test.ts
+++ tests/e2e/backend-routes/ping/ping.home.test.ts
@@ -20,19 +20,3 @@ describe('Route ping', () => {
     expect(data).toStrictEqual({ ping: 'pong' });
   });
 });
-
-describe('Route Root', () => {
-  test('GET /', async () => {
-    const service = home.environment.getContainer('api-1');
-    await service.restart();
-
-    const response = await fetch(`${home.baseUrl}/`);
-    const data = await response.json();
-
-    expect(response.status).toBe(200);
-    expect(data).toStrictEqual({
-      hello: 'world',
-      port: 3000,
-    });
-  });
-});
